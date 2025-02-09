84fd98f65c80f1c7f4faff368561da9e8b1e70a7
raghad yaseen <raghadyaseen966@gmail.com>
2025-02-09 14:34:09 +0300

    task10 2:34 9/2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tasks/tasks/task8.dart';
+import 'package:tasks/tasks/task10.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Task8(),
+      home: Task10(),
       debugShowCheckedModeBanner: false,
     );
   }
