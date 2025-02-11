3e36c939ec90c6163677233baa2593316382dbc8
raghad yaseen <raghadyaseen966@gmail.com>
2025-02-11 10:38:28 +0300

    commit 10:38 10/2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tasks/tasks/task10.dart';
+import 'package:tasks/tasks/task11.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Task10(),
-      debugShowCheckedModeBanner: false,
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: ProductListScreen());
   }
 }
