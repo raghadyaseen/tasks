089b103936f0118eb5cc6e7d65d64c55ffe36217
raghad yaseen <raghadyaseen966@gmail.com>
2025-02-27 11:54:52 +0300

    task18 27/2 11:52

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
-
-// import 'tasks/task1.dart';
-// import 'tasks/task2.dart';
 import 'package:provider/provider.dart';
-
-import 'components/task 16.dart';
-import 'providers/login_provider.dart';
-import 'providers/product_provider.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:tasks/tasks/task1.dart';
+import 'package:tasks/tasks/task17.dart';
+import 'provider/product_provder.dart';
 import 'firebase_options.dart';
-
-//import 'views/product_view_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,8 +15,7 @@ void main() async {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => ProductProvider()),
-        ChangeNotifierProvider(create: (context) => LoginProvider()),
+        ChangeNotifierProvider(create: (contextflutter) => ProductProvider()),
       ],
       child: const MyApp(),
     ),
@@ -31,19 +25,11 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      // this is task selector
-      //home: const Task1(),
-      //home: const Task2(),
-      home: Task16(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
     );
   }
 }
